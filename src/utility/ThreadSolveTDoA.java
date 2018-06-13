a44d6ba8d90e35950f255285fa4f9718da77933d
JasonBrown <qianchao7828@126.com>
2018-06-13 11:00:44 +0800

    添加了线程池

--- src/utility/ThreadSolveTDoA.java
+++ src/utility/ThreadSolveTDoA.java
@@ -1,4 +1,49 @@
 package utility;
 
-public class ThreadSolveTDoA {
+import com.mathworks.toolbox.javabuilder.MWClassID;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+import getLocation.GetLocation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class ThreadSolveTDoA implements Runnable {
+    private BufferedWriter bufferedWriter;
+    private double[] d;
+
+    public void setBufferedWriter(BufferedWriter bufferedWriter) {
+        this.bufferedWriter = bufferedWriter;
+    }
+
+    public void setD(double[] d) {
+        this.d = d;
+    }
+
+    @Override
+    public void run() {
+        /******************************************MATLAB计算定位结果***********************************************/
+        GetLocation getLocation = null;
+        Object[] rs = null;
+        MWNumericArray input = new MWNumericArray(d, MWClassID.DOUBLE);
+        try {
+            getLocation = new GetLocation();
+            rs = getLocation.getX(3, input);
+        } catch (MWException e) {
+            e.printStackTrace();
+        }
+/*******************************************输出到控制台和文件*********************************************************/
+        System.out.println("++++++++++++++++++++++++++++++++++++");
+        System.out.print("x = " + rs[0] + ", ");
+        System.out.print("y = " + rs[1] + ", ");
+        System.out.println("z = " + rs[2]);
+        System.out.println("===================================");
+        System.out.println();
+        try {
+            bufferedWriter.write(rs[0] + "   ," + rs[1] + "   ," + rs[2] + "\r\n");
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
